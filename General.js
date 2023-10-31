11a778f83384a7f6b3fc1d825d880dc3d4091841
Svyat <svyatoslavlot@gmail.com>
2023-11-01 00:17:01 +0200

    new code

--- General.js
+++ General.js
@@ -46,9 +46,83 @@ function distance() {
     let t = parseFloat(time);
     if (!isNaN(d) && !isNaN(t) && t > 0) {
         let speed = d / t;
-        alert("You need to move with speed to make it in time \t"+ speed);
+        alert("You need to move with speed to make it in time \t" + speed);
     } else { alert("Try again"); }
 }
+function convertDollarsToEuros() {
+    function converter(dollars) {
+        const exchangeRate = 0.85;
+        const euro = dollars * exchangeRate;
+        return euro;
+    }
+    let userInput = prompt("Enter sum in dollar to convertedin euro:");
+    let dollars = parseFloat(userInput);
+    if (!isNaN(dollars)) {
+        let euro = converter(dollars);
+        alert(`${dollars} dollars = ${euro} euros`)
+    } else {
+        alert("please enter right sum");
+    }
+}
+function getSize() {
+    let flashSize = prompt("Enter size of your flashDrive in GB:");
+    let flashSizeGb = parseFloat(flashSize);
+    if (!isNaN(flashSize)) {
+        let fileSize = 820;
+        let flashDriverSize = flashSizeGb * 1024;
+        let fileCount = Math.floor(flashDriverSize / fileSize);
+        alert(`In flashDriver ${flashSize} will fit ${fileCount} size 820 MB`);
+    } else { alert("Enter the right size of your flash"); }
+}
+function pocketMoney() {
+    let userPocketMoney = parseFloat(prompt("Enter how much money you have:"));
+    let costsChocolateBar = parseFloat(prompt("Enter how much one chocolate bar costs:"));
+    if (!isNaN(userPocketMoney) && !isNaN(costsChocolateBar)) {
+        if (costsChocolateBar > 0) {
+            const chocolateBarsCount = Math.floor(userPocketMoney / costsChocolateBar);
+            const change = userPocketMoney % costsChocolateBar;
+
+            if (chocolateBarsCount > 0) {
+                alert(`You can buy ${chocolateBarsCount} chocolate bars and your change will be ${change} money.`);
+            } else {
+                alert("You don't have enough money to buy a chocolate bar.");
+            }
+        } else {
+            alert("Please enter a valid cost for the chocolate bar.");
+        }
+    } else {
+        alert("Please enter valid numeric values.");
+    }
+}
+function findFourDigitPalindrome() {
+    let userInput = prompt("Enter a four-digit number:");
+    let number = parseInt(userInput);
+
+    if (!isNaN(number) && number >= 1000 && number <= 9999) {
+        let thousands = Math.floor(number / 1000);
+        let hundreds = Math.floor((number / 100) % 10);
+        let tens = Math.floor((number / 10) % 10);
+        let ones = number % 10;
+        let palindrome = ones * 1000 + tens * 100 + hundreds * 10 + thousands;
+
+        alert(`The palindrome of the number ${number} is ${palindrome}.`);
+    } else {
+        alert("Please enter a valid four-digit number.");
+    }
+}
+function checkEvenOrOdd() {
+    let userInput = prompt("Enter an number:");
+    let isEven = userInput % 2 === 0;
+    let result = isEven ? "even" : "odd";
+    alert(`You entered the number ${userInput}, which is ${result}`);
+}
+
+
+//checkEvenOrOdd();
+//findFourDigitPalindrome();
+//pocketMoney();
+//getSize();
+//convertDollarsToEuros();
 //distance();
 //radius();
 //Squere();
