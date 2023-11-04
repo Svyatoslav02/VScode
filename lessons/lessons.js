87630ca0b4ed841a6f3a8c8919a5c46caa6804e2
Svyat <svyatoslavlot@gmail.com>
2023-11-04 11:36:00 +0200

    NEW

---
+++ lessons/lessons.js
@@ -0,0 +1,54 @@
+/*block 1*/
+let textVisible = true;
+
+function toggleText() {
+    let textElement = document.getElementById('text');
+    let toggleButton = document.getElementById('Button');
+
+    if (textVisible) {
+        textElement.style.display = 'none';
+        toggleButton.innerText = 'view text';
+    } else {
+        textElement.style.display = 'block';
+        toggleButton.innerText = 'hide text';
+    }
+
+    textVisible = !textVisible;
+}
+/*block 2*/
+const arr = document.querySelectorAll('button');
+const text = ['Text  1', 'Text  2', 'Text  3'];
+const txt = document.getElementById('id');
+
+let tmp = 0;
+arr.forEach(i => {
+    i.setAttribute('txt', text[tmp]);
+    tmp++;
+    i.addEventListener('click', () => {
+        txt.innerText = i.getAttribute('txt');
+    })
+})
+/*block 3*/
+const mouseTracker = document.getElementById('mouse-tracker');
+const mouseCoordinates = document.getElementById('mouse-coordinates');
+const mouseClick = document.getElementById('mouse-click');
+
+mouseTracker.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    mouseCoordinates.innerText = `Координати мишки: X=${x}, Y=${y}`;
+});
+
+mouseTracker.addEventListener('mousedown', (e) => {
+    const button = e.button;
+    const buttonName = button === 0 ? 'ліва кнопка' : 'права кнопка';
+    mouseClick.innerText = `Натиснуто: ${buttonName}`;
+});
+/*block 4 */
+const generateButton = document.getElementById('generate-button');
+const randomNumber = document.getElementById('random-number');
+
+generateButton.addEventListener('click', () => {
+    const random = Math.floor(Math.random() * 101); // Генерує випадкове число від 0 до 100
+    randomNumber.textContent = `Випадкове число: ${random}`;
+});
